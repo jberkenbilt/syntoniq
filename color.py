299808d7d9f3d09ae89d3af8c9fc3dad3d209e91
Jay Berkenbilt <ejb@ql.org>
2025-07-13 15:37:02 -0400

    Continue iterating on color

--- color.py
+++ color.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import json
+import sys
 from operator import itemgetter
 import subprocess
 
@@ -21,7 +22,7 @@ def rgb_to_hsv(r, g, b):
 
     # Saturation
     s = 0 if mx == 0 else (diff / mx)
-    gamma = 1.0
+    gamma = 2.0
     s = s ** gamma
 
     # Brightness
@@ -29,7 +30,7 @@ def rgb_to_hsv(r, g, b):
     rx, gx, bx = 0.2126, 0.7152, 0.0722
     v = (rx * r + gx * g + bx * b)
 
-    return h, s, v
+    return int(h), int(s*255), int(v*255)
 
 
 with open('colors.json', 'r') as f:
@@ -41,12 +42,84 @@ for label, r, g, b in colors:
     hsv_colors.append((label, (h, s, v)))
 hsv_colors.sort(key=itemgetter(1), reverse=True)
 
-i = 0
-for row in range(8):
-    for col in range(8):
-        pos = 10 * (1+row) + (1+col)
-        color = hsv_colors[i][0]
-        print(pos, color)
-        pos = f'{pos:02x}'
-        subprocess.run(["./scripts/setcolor", pos, color], check=True)
-        i += 1
+
+# page: 0 or 1
+# colors: an array of 127 colors.
+# With colors=None, this should set the launchpad to match to the
+# programmer's reference, page 10.
+def draw(page, colors):
+    i = page * 64
+    for row in range(8):
+        for col in range(8):
+            pos = 10 * (8-row) + (1+col)
+            if colors is not None:
+                try:
+                    color = colors[i][0]
+                except IndexError:
+                    color = '00'
+            else:
+                color = f'{i:02x}'
+            if col > 0:
+                print(' ', end='')
+            print(color, end='')
+            pos = f'{pos:02x}'
+            subprocess.run(["./scripts/setcolor", pos, color], check=True)
+            i += 1
+        print('')
+
+
+def dump_hsv():
+    for i in sorted(hsv_colors, key=itemgetter(0)):
+        print(i)
+
+
+def dump_rgb():
+    for i in colors:
+        (label, r, g, b) = i
+        print(f'{label} #{r:02x}{g:02x}{b:02x}')
+
+
+# TODO: These need work
+
+manual_colors = [
+    # red
+    '48', '05', '78', '6A', '06', '07', '79', '00',
+    # red/orange
+    '3C', '00', '00', '00', '00', '00', '00', '00',
+    # orange
+    '54', '09', '0A', '0B', '7F', '00', '00', '00',
+    # orange/hellow
+    '7E', '3E', '00', '00', '00', '00', '00', '00',
+    # yellow
+    '0D', '0E', '0F', '7D', '7C', '3F', '00', '00',
+    # green
+    '15', '16', '17', '7B', '77', '03', '02', '01',
+    # blue
+    '4F', '29', '2A', '2B', '00', '00', '00', '67',
+    # magenta
+    '34', '35', '36', '47', '00', '00', '00', '00',
+    # purple
+    '31', '50', '45', '32', '00', '00', '00', '70',
+    # cyan
+    '4D', '21', '1E', '1F', '00', '00', '00', '00',
+]
+
+manual_colors = list([i] for i in manual_colors)
+
+to_draw = None
+try:
+    page = int(sys.argv[1])
+    which = sys.argv[2]
+    if which == 'manual':
+        to_draw = manual_colors
+    elif which == 'hsv':
+        to_draw = hsv_colors
+    else:
+        to_draw = None
+except Exception:
+    exit('Usage: color.py {0|1} {manual|hsv|none}')
+
+
+draw(page, to_draw)
+# dump_hsv()
+# dump_rgb()
